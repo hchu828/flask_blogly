0e9343083c95e08cdf7d3cf5823c46205cfec441
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-02-02 18:13:17 -0500

    Added seed

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-"""Blogly application."""
-
 from flask import Flask, request, redirect, render_template, session
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
@@ -8,6 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'Never_tell'
 
 debug = DebugToolbarExtension(app)
 
