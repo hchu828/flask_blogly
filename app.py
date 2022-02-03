38bd7d3af3a85ba66d61f0ad74ae242c58eb047e
prokid20000 <95936374+prokid20000@users.noreply.github.com>
2022-02-02 19:10:48 -0500

    userlist.html

--- app.py
+++ app.py
@@ -12,3 +12,19 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 db.create_all()
+
+@app.get('/')
+def get_home():
+    """Get to user list page(will change)"""
+    users = db.session.query(User.first_name,User.last_name).all()
+    return render_template("user_list.html", users=users)
+
+@app.get('/users')
+def get_users():
+    """Get to user list page"""
+
+    users = db.session.query(User.first_name,User.last_name).all()
+    return render_template("user_list.html", users=users)
+
+
+
